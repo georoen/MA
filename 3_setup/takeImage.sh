c4c1d14404b1d806eee821034d697a8c6a04ecc5
Jeroen Staab <jstaab@posteo.de>
2017-09-16 20:44:05 +0200

    Edited Setup 3

--- 3_setup/takeImage.sh
+++ 3_setup/takeImage.sh
@@ -1,7 +1,10 @@
-DATE=$(date +%Y%m%d-%H%M)
-#raspistill -o ./picam/picam-$DATE.jpg
-wget -O now.jpg http://webcam.rockenstein.de/cam1g.jpg  # Wuecam
-#wget -O now.jpg http://10.42.0.64/nph-jpeg.cgi  # Stardot
+FILE=img_$(date +%Y%m%d-%H%M).jpg
+#raspistill -o $FILE
+wget -O $FILE http://webcam.rockenstein.de/cam1g.jpg  # Wuecam
+#wget -O $FILE http://10.42.0.64/nph-jpeg.cgi  # Stardot
 
 # Run YOLO
-Rscript -e "wuepix::yolo_single('now.jpg')"
+Rscript -e "wuepix::yolo_single('"$FILE"')"
+
+# Remove file
+rm $FILE
