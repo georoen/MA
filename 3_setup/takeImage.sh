051f86ad8558101d5dbbe98144262ab8c65f1b73
Jee Roen <georoen@users.noreply.github.com>
2018-09-21 00:27:26 +0200

    Update takeImage.sh

--- 3_setup/takeImage.sh
+++ 3_setup/takeImage.sh
@@ -1,7 +1,14 @@
-FILE=img_$(date +%Y%m%d-%H%M).jpg
-#raspistill -o $FILE
-wget -O $FILE http://webcam.rockenstein.de/cam1g.jpg  # Wuecam
-#wget -O $FILE http://10.42.0.64/nph-jpeg.cgi  # Stardot
+# Live Processing Images with YOLO
+
+# Parameters
+cam="img_"
+url="http://webcam.rockenstein.de/cam1g.jpg"  # Wuecam
+#url="http://10.42.0.64/nph-jpeg.cgi"  # Stardot
+
+# Record Image from Source
+FILE=$cam$(date +%Y%m%d-%H%M).jpg
+wget -O $FILE $url  # Webcam
+#raspistill -o $FILE  # PiCam
 
 # Run YOLO
 Rscript -e "wuepix::yolo_single('"$FILE"')"
